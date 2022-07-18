06f809c1e6669a892d99cbd95b8b4bb1d3086690
Avinash Singh <avinashs.iitm@gmail.com>
2022-07-18 20:12:05 +0530

    Day-1

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,6 +1,7 @@
 const urlModel = require("../models/urlModel")
 const shortid = require('shortid')
 const validUrl = require('valid-url')
+const axios = require('axios')
 
 
 const isValidBody = function (x) {
@@ -20,10 +21,18 @@ const isvalid = function (x) {
 const urlShortener = async function (req, res) {
     try {
         if (!isValidBody(req.body)) return res.status(400).send({ status: false, message: "Provide the longUrl in the body." })
-        
+
         let longUrl = req.body.longUrl
         if (!isvalid(longUrl)) return res.status(400).send({ status: false, message: 'Provide the longUrl in the body. ⚠️' })
-        if (!validUrl.isUri(longUrl)) return res.status(400).send({ status: false, message: 'Enter a valid url.'})
+
+        let found = false;
+        await axios
+            .get(longUrl)
+            .then((response) => {
+                if (response.status == 200 || response.status == 201) found = true
+            })
+            .catch((error) => {})
+        if (found == false) return res.status(400).send({ status: false, message: "Enter a valid URL." })
 
         let urlPresent = await urlModel.findOne({ longUrl: longUrl })
         if (urlPresent) return res.status(400).send({ status: false, message: "URL Code is already generated for this URL." })
@@ -31,7 +40,7 @@ const urlShortener = async function (req, res) {
         let urlCode = shortid.generate(longUrl)
         let shortUrl = 'http://localhost:3000/' + urlCode
         let data = { longUrl: longUrl, shortUrl: shortUrl, urlCode: urlCode }
-        
+
         let urlGenerated = await urlModel.create(data)
         let result = { longUrl: urlGenerated.longUrl, shortUrl: urlGenerated.shortUrl, urlCode: urlGenerated.urlCode }
         return res.status(201).send({ status: true, data: result })
@@ -46,12 +55,12 @@ const urlShortener = async function (req, res) {
 
 const getUrl = async function (req, res) {
     try {
-    let urlCode = req.params.urlCode
+        let urlCode = req.params.urlCode
 
-    let urlDoc = await urlModel.findOne({ urlCode: urlCode })
-    if (!urlDoc) return res.status(404).send({ status: false, message: "URL doesn't exist. Please provide a valid URL code." })
+        let urlDoc = await urlModel.findOne({ urlCode: urlCode })
+        if (!urlDoc) return res.status(404).send({ status: false, message: "URL doesn't exist. Please provide a valid URL code." })
 
-    return res.status(200).send(`Redirecting to ${urlDoc.longUrl}`)
+        return res.status(302).redirect(urlDoc.longUrl)
     } catch (err) {
         return res.status(500).send({ status: false, message: err.message })
     }
